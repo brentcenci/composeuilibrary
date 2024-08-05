5869c490bd7292798f865db21e4da296b754aa46
Brent <bcen0001@student.monash.edu>
2024-08-06 00:10:47 +1000

    Small test using react-iframe

--- src/components/CodeSnippet.jsx
+++ src/components/CodeSnippet.jsx
@@ -2,6 +2,7 @@ import hljs from 'highlight.js'
 import kotlin from "highlight.js/lib/languages/kotlin";
 import "highlight.js/styles/monokai.css";
 import {useEffect, useRef} from "react";
+import Iframe from "react-iframe";
 
 hljs.registerLanguage("kotlin", kotlin);
 
@@ -12,6 +13,7 @@ const CodeSnippet = () => {
     }, []);
     return (
         <>
+            <Iframe url={"https://cgaisl.github.io/kodeeLogin/"} width={"800px"} height={"800px"}/>
             <pre>
                 <code className="kotlin" ref={codeRef}>
                     {
