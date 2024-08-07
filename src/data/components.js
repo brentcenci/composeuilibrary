c8bd77c076217cf193b48b74fa127f87a760b080
Brent <bcen0001@student.monash.edu>
2024-08-07 17:10:37 +1000

    Basic data for testing

---
+++ src/data/components.js
@@ -0,0 +1,42 @@
+export const componentsData = [
+    {
+        category: "Cards",
+        components: [
+            {
+                title: "Basic Card",
+                description: "Basic Card",
+                code: "code here",
+                link: "link here"
+            },
+            {
+                title: "Basic Card with Image",
+                description: "Basic Card with Image",
+                code: "code here",
+                link: "link here"
+            },
+            {
+                title: "Card with Badges",
+                description: "Card with Badges",
+                code: "code here",
+                link: "link here"
+            },
+            {
+                title: "Video Card",
+                description: "Video Card",
+                code: "code here",
+                link: "link here"
+            }
+        ]
+    },
+    {
+        category: "Authentication",
+        components: [
+            {
+                title: "Basic Authentication",
+                description: "Basic Authentication",
+                code: "code here",
+                link: "link here"
+            }
+        ]
+    }
+];
