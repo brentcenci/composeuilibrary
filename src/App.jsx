78a22d623113dbf2fa66b2bf321dd375deeb7699
Brent <bcen0001@student.monash.edu>
2024-08-08 11:48:55 +1000

    Basic TopNavBar with FlowBite interactivity implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import CodeSnippet from "./components/CodeSnippet.jsx";
-import HomeScreen from "./screens/HomeScreen.jsx";
-import Card from "./components/Card.jsx";
+import TopNavBar from "./components/TopNavBar.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-        <Card/>
-        <h1>Vite + React</h1>
-        <CodeSnippet/>
-        <HomeScreen/>
+        <TopNavBar/>
+
     </>
   )
 }
---
+++ src/components/TopNavBar.jsx
@@ -0,0 +1,36 @@
+const TopNavBar = () => {
+    return (
+        <nav className="border-b-gray-200 bg-white">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between p-4">
+                <a href="/">
+                    <p>Logo Goes Here</p>
+                </a>
+                <button
+                    data-collapse-toggle="navbar-default"
+                    type="button"
+                    className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden">
+                    <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                         viewBox="0 0 17 14">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                              d="M1 1h15M1 7h15M1 13h15"/>
+                    </svg>
+                </button>
+                <div className="hidden md:block w-full md:w-auto" id="navbar-default">
+                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-6 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
+                        <li>
+                            Home
+                        </li>
+                        <li>
+                            Components
+                        </li>
+                        <li>
+                            Screens
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default TopNavBar;
