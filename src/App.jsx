0af5e376bda774f1cd344650f9c6108857612088
Brent <bcen0001@student.monash.edu>
2024-08-08 12:08:38 +1000

    Basic HeaderSection implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import TopNavBar from "./components/TopNavBar.jsx";
+import HeaderSection from "./components/homescreencomponents/HeaderSection.jsx";
 
 function App() {
 
   return (
     <>
         <TopNavBar/>
-
+        <HeaderSection/>
     </>
   )
 }
---
+++ src/components/homescreencomponents/HeaderSection.jsx
@@ -0,0 +1,10 @@
+const HeaderSection = () => {
+    return (
+        <div className="text-center w-4/6 bg-white mx-auto my-10 space-y-4 text-gray-900">
+            <h1 className="text-5xl font-BowlbyOne">Ready-built Components for Compose</h1>
+            <h2 className="text-xl">Browse this free, open-source collection of reusable components and sample screen designs for any Android or Compose Multiplatform projects you are undertaking</h2>
+        </div>
+    );
+};
+
+export default HeaderSection;
