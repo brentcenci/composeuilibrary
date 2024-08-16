5f0d694e134e735038735058a2a54eb5fbf8869e
Brent <bcen0001@student.monash.edu>
2024-08-17 01:27:51 +1000

    Adjusting spacing for the header section

--- src/components/homescreencomponents/HeaderSection.jsx
+++ src/components/homescreencomponents/HeaderSection.jsx
@@ -1,13 +1,13 @@
 const HeaderSection = () => {
     return (
         <>
-            <div className="text-center w-4/6 lg:w-1/2 bg-white mx-auto my-10 space-y-4 text-gray-900">
+            <div className="text-center w-4/6 lg:w-1/2 bg-white mx-auto my-40 space-y-4 text-gray-900">
                 <h1 className="text-5xl font-BowlbyOne">Ready-built Components for <span
                     className="text-blue-700 hover:text-blue-500">Compose</span></h1>
                 <h2 className="text-xl">Browse this free, open-source library of reusable components and sample screen
                     designs for any Android or Compose Multiplatform projects you are undertaking</h2>
             </div>
-            <div className="flex flex-row justify-center space-x-4 md:space-x-12 mx-auto md:mx-20 py-10 text-gray-900">
+            <div className="flex flex-row justify-center space-x-4 md:space-x-12 mx-auto md:mx-20 pb-20 text-gray-900">
                 <div className="flex flex-row max-w-80">
                     <h1 className="text-7xl font-BowlbyOne text-blue-700">1</h1>
                     <div className="flex flex-col">
