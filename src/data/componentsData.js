43ae40097880b70d034624b1f934c365162c100e
Brent <bcen0001@student.monash.edu>
2024-08-22 16:51:41 +1000

    Updated the Comments / Reviews section

--- src/data/componentsData.js
+++ src/data/componentsData.js
@@ -9,7 +9,7 @@ export const componentsData = [
                 description: "This is a standard card design to get you started, featuring a title and a brief body of text. " +
                     "Use this to describe simple articles or repeated features that do not require too much attention.",
                 code:
-`@Composable
+                    `@Composable
 fun Card(modifier: Modifier = Modifier, title: String, content: String) {
     Box(modifier = Modifier.background(Color.White, RoundedCornerShape(20.dp)).width(300.dp).padding(20.dp)) {
         Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
@@ -26,7 +26,7 @@ fun Card(modifier: Modifier = Modifier, title: String, content: String) {
                 description: "This is the same standard card design but now with an image header, to display a relevant photograph or image. " +
                     "Use this in applications where you want to grab the user's attention with a charming image.",
                 code:
-`@Composable
+                    `@Composable
 fun ImageCard(title: String, content: String, imageUrl: String) {
     Box(modifier = Modifier.background(Color.White, RoundedCornerShape(20.dp)).width(300.dp).padding(20.dp)) {
         Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
@@ -51,7 +51,7 @@ fun ImageCard(title: String, content: String, imageUrl: String) {
                     "such as whether the card is depicting a new release item, a sale, or even categories such as vegan or vegetarian. " +
                     "Use this to call to attention these properties on the main card.",
                 code:
-`@Composable
+                    `@Composable
 fun ImageCardWithBadges(title: String, content: String, imageUrl: String, titleBadge: String, contentBadges: List<String>) {
     Box(modifier = Modifier.background(Color.White, RoundedCornerShape(20.dp)).width(300.dp).padding(20.dp)) {
         Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
@@ -88,7 +88,7 @@ fun ImageCardWithBadges(title: String, content: String, imageUrl: String, titleB
                 description: "This card is the same as the image card however now features a simple but attention-grabbing button, " +
                     "perfect for any desired call to action.",
                 code:
-`@Composable
+                    `@Composable
 fun ImageCardWithButton(title: String, content: String, imageUrl: String, buttonText: String) {
     Box(modifier = Modifier.background(Color.White, RoundedCornerShape(20.dp)).width(300.dp).padding(20.dp)) {
         Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
@@ -116,7 +116,7 @@ fun ImageCardWithButton(title: String, content: String, imageUrl: String, button
                     "where each is just a quick snapshot. " +
                     "Specific use cases could be for menu applications to depict a thumbnail of the food, with a title and brief description.",
                 code:
-`@Composable
+                    `@Composable
 fun CardWithSideImage(title: String, content: String, imageUrl: String) {
     Box(modifier = Modifier.background(Color.White, RoundedCornerShape(20.dp)).width(300.dp)) {
         Row(modifier = Modifier.clip(RoundedCornerShape(20.dp))) {
@@ -142,7 +142,7 @@ fun CardWithSideImage(title: String, content: String, imageUrl: String) {
                     "with a thumbnail image, time-stamp, title and statistics summary text. " +
                     "This can be of use if you plan to incorporate video browsing into your application.",
                 code:
-`@Composable
+                    `@Composable
 fun VideoCardYoutubeStyle(title: String, authorName: String, authorImage: String, views: String, time: String, length: String, imageUrl: String) {
     Box(modifier = Modifier.background(Color.White, RoundedCornerShape(20.dp)).padding(20.dp)) {
         Column(modifier = Modifier.width(400.dp)) {
@@ -186,7 +186,7 @@ fun VideoCardYoutubeStyle(title: String, authorName: String, authorImage: String
                 description: "An alternative video card design which depicts the length of the video, the author/owner of the video, as well as a title and brief description. " +
                     "Use this in your project or just take inspiration to design your own video cards to use if your applications!",
                 code:
-`@Composable
+                    `@Composable
 fun VideoCard(title: String, summary: String, length: String, imageUrl: String, authorImage: String, authorName: String) {
     Box(modifier = Modifier.background(Color.White, RoundedCornerShape(20.dp))) {
         Column(modifier = Modifier.width(400.dp)) {
@@ -230,12 +230,14 @@ fun VideoCard(title: String, summary: String, length: String, imageUrl: String, 
     },
     {
         category: "Comments and Reviews",
-        categoryDescription: "Small, interactive elements representing tags, categories, or actions, often used for filtering or selection in a compact format.",
+        categoryDescription: "A selection of complete components and elements that may be found in comment sections or review areas of an application. " +
+            "They should be used to provide the opportunity for users to give their thoughts on a piece of content or media.",
         thumbnail: "src/assets/ThumbnailCommentsReview.png",
         components: [
             {
                 title: "Basic Comment",
-                description: "Description for a Basic Comment Component",
+                description: "This is a basic comment component where users can leave a simple post behind on media. " +
+                    "The component features a user icon image, textfield and submission button.",
                 code: `
 @Composable
 fun ExampleComponent(modifier: Modifier) {
@@ -249,7 +251,9 @@ fun ExampleComponent(modifier: Modifier) {
             },
             {
                 title: "Alternative Comment",
-                description: "Description for a Alternative Comment Component",
+                description: "This is an alternative to the above basic comment component, where the username and user icon image are both displayed. " +
+                    "This should be used in the same scenario as above, where the user is leaving a comment behind on a piece of media. " +
+                    "You may want to use this to highlight the user's details, and as such should be paired with a posted comment component featuring such details.",
                 code: `
 @Composable
 fun ExampleComponent(modifier: Modifier) {
@@ -263,7 +267,9 @@ fun ExampleComponent(modifier: Modifier) {
             },
             {
                 title: "Star Rating Bar",
-                description: "Description for a Star Rating Bar",
+                description: "This is a simple example of a Star Rating Bar component, in which the user can select a rating between 1 and 5 stars. " +
+                    "The colours, size and spacing etc. are customizable using the above component, and further customization can be achieved by altering this code. " +
+                    "This sort of feature fits in any kind of review scenario, where you need to query what rating a user would give toward a product or service.",
                 code: `
 @Composable
 fun ExampleComponent(modifier: Modifier) {
@@ -277,7 +283,9 @@ fun ExampleComponent(modifier: Modifier) {
             },
             {
                 title: "Review Post with Star Rating Bar",
-                description: "Description for a Review Post with Star Rating Bar",
+                description: "This is a sample component featuring a star rating bar, " +
+                    "as well as basic information allowing the user to enter both a rating and a custom comment attached to their review, and then submit." +
+                    "This kind of component may be particularly valuable as a modal, which appears after the user has made a purchase or completed some kind of media.",
                 code: `
 @Composable
 fun ExampleComponent(modifier: Modifier) {
