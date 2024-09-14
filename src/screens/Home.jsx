1bc6d4048d492a5f622189b0083a4e1812ae50e5
Brent <bcen0001@student.monash.edu>
2024-09-14 16:38:36 +1000

    Added extra button, but not sure if I'll keep

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -7,10 +7,17 @@ const Home = () => {
                         <h1 className="font-black text-5xl md:text-6xl lg:text-7xl xl:text-8xl">Ready-built <br/> components for</h1>
                         <h1 className="font-black text-6xl md:text-7xl lg:text-8xl xl:text-9xl tracking-tighter bg-blue-gradient bg-left bg-[length:200%_200%] hover:bg-right transition-all duration-700 ease-in-out inline text-transparent bg-clip-text pe-3">Compose</h1>
                         <br/>
-                        <button
-                            className="mt-40 bg-blue-gradient bg-left bg-[length:200%_200%] hover:bg-right transition-all duration-700 ease-in-out py-2 md:py-3 lg:py-5 xl:py-7 px-4 md:px-6 lg:px-8 xl:px-10 rounded-full text-white tracking-tight font-bold text-lg md:text-xl lg:text-2xl xl:text-3xl">View
-                            Components »
-                        </button>
+                        <div className="flex space-x-4">
+                            <button
+                                className="mt-40 bg-blue-gradient bg-left bg-[length:200%_200%] hover:bg-right transition-all duration-700 ease-in-out py-2 md:py-3 lg:py-5 xl:py-7 px-4 md:px-6 lg:px-8 xl:px-10 rounded-full text-white tracking-tight font-bold text-lg md:text-xl lg:text-2xl xl:text-3xl">
+                                Get Access »
+                            </button>
+                            <button
+                                className="mt-40 bg-blue-gradient bg-left bg-[length:200%_200%] hover:bg-right transition-all duration-700 ease-in-out py-2 md:py-3 lg:py-5 xl:py-7 px-4 md:px-6 lg:px-8 xl:px-10 rounded-full text-white tracking-tight font-bold text-lg md:text-xl lg:text-2xl xl:text-3xl">
+                                View Components »
+                            </button>
+                        </div>
+
                     </div>
                     <div className="hidden md:block md:w-1/4">
                         <img
