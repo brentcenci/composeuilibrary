803c38510e28285e4b5ad7f27647d9bfa8fb5107
Brent <bcen0001@student.monash.edu>
2024-09-16 01:02:36 +1000

    Tried to design a dotted trail leading through the steps section but does not look right.

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -59,8 +59,16 @@ const Home = () => {
             </div>
 
             <div id="steps">
+                {/*<div className="w-full h-full -z-10 flex justify-center absolute">
+                    <svg className="" viewBox="0 0 68 123" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M36.0001 0.5C36.0001 6.5 -44.342 17.3495 36.0001 42C80 55.5 77.9238 60.9762 36.0001 74.5C-41.5 99.5 36.0001 102.5 36.0001 123"
+                            stroke="#a3a3a3" stroke-dasharray="5 5"/>
+                    </svg>
+
+                </div>*/}
                 <p className="text-center text-2xl mb-40">As easy as...</p>
-                <div className="flex-col w-3/4 lg:w-1/2 mx-auto space-y-36">
+                <div className="flex-col w-3/4 xl:w-1/2 mx-auto space-y-36">
                     <div className="flex">
                         <div className="flex flex-row w-3/5">
                             <h1 className="font-black text-9xl bg-blue-gradient bg-left bg-[length:200%_200%] hover:bg-right transition-all duration-700 ease-in-out inline text-transparent bg-clip-text px-5">
